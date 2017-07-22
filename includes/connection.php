fda35425b73bd6f81ebe1b70eba6c9d13e546b27
Anirban Dutta <anirbandutta011@gmail.com>
2017-07-22 13:47:23 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost","root","server24","notes" ) or die ("error" . mysqli_error($conn));
+$conn = mysqli_connect("localhost","root","","notes" ) or die ("error" . mysqli_error($conn));
 ?>
